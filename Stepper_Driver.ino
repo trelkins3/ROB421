0cc31b0e3cb479ae11426e75f3302bdd0f939068
Caleb Hubbell <35707865+hubbellc@users.noreply.github.com>
2018-05-02 17:35:59 -0700

    Add files via upload

---
+++ Stepper_Driver.ino
@@ -0,0 +1,42 @@
+int PUL=3; //define Pulse pin
+int DIR=4; //define Direction pin
+int Limit = 23; //define Enable Pin
+
+//accelstepper
+
+int timeDel = 100;
+int steps = 1400;
+
+void setup() {
+  pinMode (PUL, OUTPUT);
+  pinMode (DIR, OUTPUT);
+  pinMode (Limit, INPUT);
+
+}
+
+void loop() 
+{
+  Serial.println(digitalRead(Limit));
+  while(digitalRead(Limit) == LOW)
+  {
+    digitalWrite(DIR,LOW);
+    digitalWrite(PUL,HIGH);
+    delayMicroseconds(timeDel);
+    digitalWrite(PUL,LOW);
+    delayMicroseconds(timeDel);
+  }
+
+  if(digitalRead(Limit) == HIGH)
+  {
+    delay(5000);
+    for (int i=0; i<steps; i++)
+    {
+      digitalWrite(DIR,HIGH);
+      digitalWrite(PUL,HIGH);
+      delayMicroseconds(timeDel);
+      digitalWrite(PUL,LOW);
+      delayMicroseconds(timeDel);
+    }
+    delay(5000);
+  }
+}
