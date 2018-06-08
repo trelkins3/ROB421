79670115286574395261616ba4b5bb09aa80fa61
Kevin <greenkev@oregonstate.edu>
2018-06-08 09:09:09 -0700

    Final Code for comp and demos

--- Arduino/PPIDriveBase/PPIDriveBase.ino
+++ Arduino/PPIDriveBase/PPIDriveBase.ino
@@ -2,10 +2,9 @@
 #include <Encoder.h>
 #include <math.h>
 #include <AccelStepper.h>
-#include <i2c_t3.h>
 
-#define MAX_VEL 40
-#define MAX_ROT_VEL 0.5
+#define MAX_VEL 60
+#define MAX_ROT_VEL 2
 #define R_BODY 15.748
 
 #define KP 20.00
@@ -41,22 +40,6 @@
 // 537.6 steps per output revolution
 #define STEP_TO_DIST 0.01899214308
 
-// gross stuff for i2c comm
-#define TCS34725_ADDRESS          (0x29)
-#define TCS34725_COMMAND_BIT      (0x80)
-#define TCS34725_ENABLE           (0x00)
-#define TCS34725_ENABLE_AEN       (0x02)    /* RGBC Enable - Writing 1 actives the ADC, 0 disables it */
-#define TCS34725_ENABLE_PON       (0x01)    /* Power on - Writing 1 activates the internal oscillator, 0 disables it */                   
-#define TCS34725_ID               (0x12)    /* 0x44 = TCS34721/TCS34725, 0x4D = TCS34723/TCS34727 */
-#define TCS34725_CDATAL           (0x14)    /* Clear channel data */
-#define TCS34725_CDATAH           (0x15)
-#define TCS34725_RDATAL           (0x16)    /* Red channel data */
-#define TCS34725_RDATAH           (0x17)
-#define TCS34725_GDATAL           (0x18)    /* Green channel data */
-#define TCS34725_GDATAH           (0x19)
-#define TCS34725_BDATAL           (0x1A)    /* Blue channel data */
-#define TCS34725_BDATAH           (0x1B)
-#define alpha  100
 
 double des_forward_vel = 0;
 double des_right_vel = 0;
@@ -87,16 +70,6 @@ unsigned long loopPeriod;
 
 String inData = "";
 
-// i2c vars
-unsigned long r; 
-unsigned long rBase[3]; 
-unsigned long g; 
-unsigned long gBase[3];
-unsigned long b; 
-unsigned long bBase[3]; 
-unsigned long c;
-unsigned long cBase[3]; 
-bool line[3] = {false,false,false}; 
 
 enum throwerMechanismState{
 readyToThrow,
@@ -141,71 +114,22 @@ void setup() {
   _Enc1_Prev_Count = 0;
   _Enc2_Prev_Count = 0;
   _Enc3_Prev_Count = 0;
-
-  // i2c initialization
-  beg();
-  
-  // initialize the RGB sensors
-  for (int i = 0; i < 3; i++)
-  {
-    write8(TCS34725_ENABLE, TCS34725_ENABLE_PON, i);
-    delay(3);
-    write8(TCS34725_ENABLE, TCS34725_ENABLE_PON | TCS34725_ENABLE_AEN, i);
-  }
-
-  //get initial values
-  for(int i = 0; i < 3; i++)
-  {
-    getRawData(&r, &g, &b, &c, i);
-    rBase[i] = r;
-    gBase[i] = g;
-    bBase[i] = b;
-    cBase[i] = c;
-  }
 }
 
 void loop() {
   parseSerial();
   updateVelocityEstimates();
   updateDriveMotors();
-  updateLine();
   updateThrower();
   
   digitalWrite(STEPPER_ENABLE_PIN,!digitalRead(MOTOR_ENABLE_INPUT));
-  //des_forward_vel = 0.999*des_forward_vel;
-  //des_right_vel = 0.999*des_right_vel;
-  //des_rotational_vel = 0.999*des_rotational_vel;
+  des_forward_vel = 0.999*des_forward_vel;
+  des_right_vel = 0.999*des_right_vel;
+  des_rotational_vel = 0.999*des_rotational_vel;
 }
 
 void updateDriveMotors()
-{
-  // check for line
-  if(line[1] == true) // front line
-  {
-    des_forward_vel = max(des_forward_vel,0);
-    line[1] = false;
-  }
-
-  if(line[2] == true) // right line
-  {
-    des_right_vel =  0.5 * (0.5*des_right_vel + 0.8660254*des_forward_vel) + 
-                    -0.8660254 * max(-0.8660254*des_right_vel + 0.5*des_forward_vel,0);
-
-    des_forward_vel =  0.8660254 * (0.5*des_right_vel + 0.8660254*des_forward_vel) + 
-                       0.5 * max(-0.8660254*des_right_vel + 0.5*des_forward_vel,0);
-    line[2] = false;
-  }
-
-  if(line[0] == true) // left line
-  {
-    des_right_vel =  0.5 * (0.5*des_right_vel - 0.8660254*des_forward_vel) + 
-                    0.8660254 * max(0.8660254*des_right_vel + 0.5*des_forward_vel,0);
-
-    des_forward_vel =  -0.8660254 * (0.5*des_right_vel - 0.8660254*des_forward_vel) + 
-                       0.5 * max(0.8660254*des_right_vel + 0.5*des_forward_vel,0);
-    line[0] = false;
-  }
-  
+{  
   double motor_1_speed = (0.86602*des_forward_vel + 0.5*des_right_vel + R_BODY*des_rotational_vel);
   double motor_2_speed = (0.0*des_forward_vel - 1*des_right_vel + R_BODY*des_rotational_vel);
   double motor_3_speed = (-0.86602*des_forward_vel + 0.5*des_right_vel + R_BODY*des_rotational_vel);
@@ -349,9 +273,9 @@ void updateThrower(){
     //Serial.println("Stop Throw");
     throwState = stopping;
   }
-  else if( throwState == stopping && millis() - throwerTimer > 1000 ){
+  else if( throwState == stopping && millis() - throwerTimer > 400 ){
     stepper.setAcceleration(1000); 
-    stepper.setMaxSpeed(300);
+    stepper.setMaxSpeed(1000);
     stepper.moveTo(40000);
     
     //Serial.println("Began Retracting");
@@ -361,6 +285,7 @@ void updateThrower(){
     stepper.setAcceleration(600000);
     stepper.stop(); // Stop as fast as possible: sets new target
     stepper.setCurrentPosition(0); //Reset stepper count to zero
+    stepper.moveTo(-100);
 
     //Serial.println("Retracted");
     throwState = readyToThrow;
@@ -369,40 +294,6 @@ void updateThrower(){
 
   //Update the stepper Command
   stepper.run();
-}
-
-void updateLine()
-{
-  // loop through and get the data from each sensor
-  for(int i = 0; i < 3; i++)
-  {
-    getRawData(&r, &g, &b, &c, i);
-  
-//    Serial.print("BASE:\t"); Serial.print(i);
-//    Serial.print("\tR:\t"); Serial.print(rBase[i]);
-//    Serial.print("\tG:\t"); Serial.print(gBase[i]);
-//    Serial.print("\tB:\t"); Serial.print(bBase[i]);
-//    Serial.print("\tC:\t"); Serial.println(cBase[i]);
-
-//    Serial.print("Sensor:\t"); Serial.print(i);
-//    Serial.print("\tR:\t"); Serial.print(r);
-//    Serial.print("\tG:\t"); Serial.print(g);
-//    Serial.print("\tB:\t"); Serial.print(b);
-//    Serial.print("\tC:\t"); Serial.println(c);
-
-    //check for line
-    if((r > rBase[i] + 1000) && (g < gBase[i] - 1000) && (b < bBase[i] - 1000) && c > (cBase[i] + 200)) // Modify values here
-    {
-      line[i] = true;
-      Serial.print("Line found on:\t"); Serial.println(i + 1);
-    }
-
-    // update running average
-    rBase[i] = (alpha * r + (256 - alpha) * rBase[i])/ 256;
-    gBase[i] = (alpha * g + (256 - alpha) * gBase[i])/ 256;
-    bBase[i] = (alpha * b + (256 - alpha) * bBase[i])/ 256;
-    cBase[i] = (alpha * c + (256 - alpha) * cBase[i])/ 256;
-  }  
 }
 
 /**************************************************************************/
@@ -435,155 +326,5 @@ double getFeedForward(double velocity, int motor){
 }
 double sign(double value) { 
  return double((value>0)-(value<0)); 
-}
-
-//Initializes I2C and configures the sensor (call this function before
-//  doing anything else)
-void beg(void) 
-{
-  // initalize scl/sda
-  Wire.begin(I2C_MASTER, 0x00, I2C_PINS_18_19, I2C_PULLUP_EXT, 4000);
-  Wire.setDefaultTimeout(20000);
-
-  Wire1.begin(I2C_MASTER, 0x00, I2C_PINS_37_38, I2C_PULLUP_EXT, 4000);
-  Wire1.setDefaultTimeout(20000);
-
-  Wire2.begin(I2C_MASTER, 0x00, I2C_PINS_3_4, I2C_PULLUP_EXT, 4000);
-  Wire2.setDefaultTimeout(20000);
-  
-  /* Make sure we're actually connected */
-  uint8_t x = read8(TCS34725_ID, 0);
-  uint8_t y = read8(TCS34725_ID, 1);
-  uint8_t z = read8(TCS34725_ID, 2);
-
-  if (((x != 0x44) && (x != 0x10)) || ((y != 0x44) && (y != 0x10)) || ((z != 0x44) && (z != 0x10)))
-  {
-      Serial.print("TCS34725 ERROR: ");
-      
-      // traceback checking each read independently
-      if((x != 0x44) && (x != 0x10))
-      {
-        Serial.println("on 1.");
-      }
-      if((y != 0x44) && (y != 0x10))
-      {
-        Serial.println("on 2.");
-      }
-      if((z != 0x44) && (z != 0x10))
-      {
-        Serial.println("on 3.");
-      }
-  }
-}
-//Reads the raw red, green, blue and clear channel values
-void getRawData (unsigned long *r, unsigned long *g, unsigned long *b, unsigned long *c, int i)
-{
-  *c = read16(TCS34725_CDATAL, i);
-  *r = read16(TCS34725_RDATAL, i);
-  *g = read16(TCS34725_GDATAL, i);
-  *b = read16(TCS34725_BDATAL, i);
-}
-//Writes a register and an 8 bit value over I2C
-void write8 (uint8_t reg, uint32_t value, int i)
-{
-  if (i == 0)
-  {
-    Wire.beginTransmission(TCS34725_ADDRESS);
-    Wire.write(TCS34725_COMMAND_BIT | reg);
-    Wire.write(value & 0xFF);
-    Wire.endTransmission();
-}
-
-  if (i == 1)
-  {
-    Wire1.beginTransmission(TCS34725_ADDRESS);
-    Wire1.write(TCS34725_COMMAND_BIT | reg);
-    Wire1.write(value & 0xFF);
-    Wire1.endTransmission();
-  }
-
-  if (i == 2)
-  {
-    Wire2.beginTransmission(TCS34725_ADDRESS);
-    Wire2.write(TCS34725_COMMAND_BIT | reg);
-    Wire2.write(value & 0xFF);
-    Wire2.endTransmission();
-  }
-}
-//Reads an 8 bit value over I2C
-uint8_t read8(uint8_t reg, int i)
-{
-  if (i == 0)
-  {
-    Wire.beginTransmission(TCS34725_ADDRESS);
-    Wire.write(TCS34725_COMMAND_BIT | reg);
-    Wire.endTransmission();
-    Wire.requestFrom(TCS34725_ADDRESS, 1);
-    return Wire.read();
-  }
-
-  if (i == 1)
-  {
-    Wire1.beginTransmission(TCS34725_ADDRESS);
-    Wire1.write(TCS34725_COMMAND_BIT | reg);
-    Wire1.endTransmission();
-    Wire1.requestFrom(TCS34725_ADDRESS, 1);
-    return Wire1.read();
-  }
-
-  if (i == 2)
-  {
-    Wire2.beginTransmission(TCS34725_ADDRESS);
-    Wire2.write(TCS34725_COMMAND_BIT | reg);
-    Wire2.endTransmission();
-    Wire2.requestFrom(TCS34725_ADDRESS, 1);
-    return Wire2.read();
-  }
-  else
-    return 0;
-}
-//Reads a 16 bit values over I2C
-unsigned long read16(uint8_t reg, int i)
-{
-  long x; long t;
-
-  if (i == 0)
-  {
-    Wire.beginTransmission(TCS34725_ADDRESS);
-    Wire.write(TCS34725_COMMAND_BIT | reg);
-    Wire.endTransmission();
-    Wire.requestFrom(TCS34725_ADDRESS, 2);
-    t = Wire.read();
-    x = Wire.read();
-  }
-
-  else if (i == 1)
-  {
-    Wire1.beginTransmission(TCS34725_ADDRESS);
-    Wire1.write(TCS34725_COMMAND_BIT | reg);
-    Wire1.endTransmission();
-    Wire1.requestFrom(TCS34725_ADDRESS, 2);
-    t = Wire1.read();
-    x = Wire1.read();
-  }
-
-  else if (i == 2)
-  {
-    Wire2.beginTransmission(TCS34725_ADDRESS);
-    Wire2.write(TCS34725_COMMAND_BIT | reg);
-    Wire2.endTransmission();
-    Wire2.requestFrom(TCS34725_ADDRESS, 2);
-    t = Wire2.read();
-    x = Wire2.read();
-  }
-  else
-  {
-    t = 0;
-    x = 0;
-  }
-
-  x <<= 8;
-  x |= t;
-  return x;
 }
 
